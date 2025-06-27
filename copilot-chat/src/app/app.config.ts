94b1dd0f3bea62e942dcb06fe636d9d29bd143ae
Nimeth Fernando <nimeth42@gmail.com>
2025-06-27 10:24:55 +0530

    HTTP and Forms support

--- copilot-chat/src/app/app.config.ts
+++ copilot-chat/src/app/app.config.ts
@@ -1,11 +1,15 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import { provideForms } from '@angular/forms';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideHttpClient(),
+    provideForms(),
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes), provideClientHydration(withEventReplay())
